adc80a57df8da5b0c8a4d44aece5248b7e3c03d0
maybexss <maybe_xss@163.com>
2019-06-01 12:55:43 +0800

    init

---
+++ src/main/java/ServletDemo.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/doServletDemo")
+public class ServletDemo extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.sendRedirect("https://github.com/maybexss");
+    }
+}
